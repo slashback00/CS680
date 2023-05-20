c5bfc4db2a278a24f20649fab70e9f4f5ba03837
Atharva Sachin Khadgi <69451310+slashback00@users.noreply.github.com>
2023-05-19 22:24:43 -0400

    Delete hw14 directory

--- hw14/src/edu/umb/cs680/hw14/fs/FSVisitor.java
+++
@@ -1,7 +0,0 @@
-package edu.umb.cs680.hw14.fs;
-
-public interface FSVisitor {
-    public void visit(Link link); //Link visitor method
-    public void visit(Directory dir); //Directory visitor method
-    public void visit(File file); //File visitor method
-}
