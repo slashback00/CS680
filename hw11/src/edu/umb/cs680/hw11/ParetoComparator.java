041886df52b68107a234f71ced63a9aedbb92c8b
Atharva Sachin Khadgi <69451310+slashback00@users.noreply.github.com>
2023-05-19 22:24:07 -0400

    Delete hw11 directory

--- hw11/src/edu/umb/cs680/hw11/ParetoComparator.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw11;
-
-import java.util.Comparator;
-
-public class ParetoComparator implements Comparator<Car>{
-    public int compare(Car car1, Car car2) {
-		return car1.getDominationCount() - car2.getDominationCount();
-	}
-}
