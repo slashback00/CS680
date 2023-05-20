cd3978f5d4b737d0ef9ba0836349fee742a2a5a2
Atharva Sachin Khadgi <69451310+slashback00@users.noreply.github.com>
2023-05-19 22:25:04 -0400

    Delete hw16 directory

--- hw16/src/edu/umb/cs680/hw16/DjiAapp/Observer.java
+++
@@ -1,5 +0,0 @@
-package edu.umb.cs680.hw16.DjiAapp;
-
-public interface Observer<T> {
-	public void update(Observable<T> sender, T event);
-}
--- hw16/src/edu/umb/cs680/hw16/DjiAapp/ThreeDObserver.java
+++
@@ -1,9 +0,0 @@
-package edu.umb.cs680.hw16.DjiAapp;
-
-public class ThreeDObserver implements Observer<Double>{
-
-    @Override
-    public void update(Observable<Double> sender, Double event) {
-        System.out.println("ThreeDObserver: " + event);
-    }
-}
